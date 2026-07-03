2d02bc97bd418cea6e2bedbfcb455e46819b4b1d
theashupandit <ashwinikumaramar787@gmail.com>
2026-07-03 13:59:28 +0530

    Google Analytics v1.5.7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,6 +115,28 @@ export default async function RootLayout({
   if (isMaintenance) {
     return (
       <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable, playfair.variable)}>
+        <head>
+          {/* Google Tag Manager */}
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-54DLGX75');
+            `
+          }} />
+          {/* Google Analytics (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-TPG09B517M" />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TPG09B517M');
+            `
+          }} />
+        </head>
         <body className={cn(inter.className, "overflow-x-hidden w-full bg-neutral-950 text-white")} suppressHydrationWarning>
           {/* Google Tag Manager (noscript) */}
           <noscript>
@@ -171,29 +193,7 @@ export default async function RootLayout({
             </div>
             <ThemeToaster />
           </ThemeProvider>
-          {/* Google Tag Manager */}
-          <Script id="gtm-maintenance" strategy="beforeInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-54DLGX75');
-            `}
-          </Script>
-          {/* Google Analytics (gtag.js) */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-TPG09B517M"
-            strategy="beforeInteractive"
-          />
-          <Script id="google-analytics-maintenance" strategy="beforeInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-TPG09B517M');
-            `}
-          </Script>
+
           {/* Pinterest Tag */}
           <Script id="pinterest-tag-maintenance" strategy="afterInteractive">
             {`
@@ -234,6 +234,28 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable, playfair.variable)}>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-54DLGX75');
+          `
+        }} />
+        {/* Google Analytics (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TPG09B517M" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TPG09B517M');
+          `
+        }} />
+      </head>
       <body className={cn(inter.className, "overflow-x-hidden w-full")} suppressHydrationWarning>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -273,29 +295,7 @@ export default async function RootLayout({
             <CookieConsent />
           </SmoothScroll>
         </ThemeProvider>
-        {/* Google Tag Manager */}
-        <Script id="gtm" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-54DLGX75');
-          `}
-        </Script>
-        {/* Google Analytics (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-TPG09B517M"
-          strategy="beforeInteractive"
-        />
-        <Script id="google-analytics" strategy="beforeInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-TPG09B517M');
-          `}
-        </Script>
+
         {/* Pinterest Tag */}
         <Script id="pinterest-tag" strategy="afterInteractive">
           {`
